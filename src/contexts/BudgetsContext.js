cd07cd7a3c4cb98f0ca07500cb35e0c2cf8c62df
Artem Vlasiuk <artemvlasiuk93@gmail.com>
2022-10-06 20:41:15 +0300

    project finished

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,7 +1,10 @@
 import React, { useContext, useState } from 'react';
 import { v4 as uuidV4 } from 'uuid';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const BudgetsContext = React.createContext()
+
+export const UNCATEGORIZED_BUDGET_ID = 'Uncategorized';
 
 export function useBudgets() {
     return useContext(BudgetsContext)
@@ -9,8 +12,8 @@ export function useBudgets() {
 
 export const BudgetsProvider = ({ children }) => {
 
-    const [budgets, setBudgets] = useState([]);
-    const [expenses, setExpenses] = useState([]);
+    const [budgets, setBudgets] = useLocalStorage('budgets', []);
+    const [expenses, setExpenses] = useLocalStorage('expenses', []);
 
     function getBudgetExpenses(budgetId) {
         return expenses.filter(expense => expense.budgetId === budgetId)
@@ -31,6 +34,12 @@ export const BudgetsProvider = ({ children }) => {
     }
     function deleteBudget({id}) {
         // TODO: Deal with expenses
+        setExpenses(prevExpenses => {
+            return prevExpenses.map(expense => {
+                if (expense.budgetId !== id) return expense;
+                return {...expense, budgetId: UNCATEGORIZED_BUDGET_ID}
+            })
+        })
 
         setBudgets(prevBudgets => {
             return prevBudgets.filter(budget => budget.id !== id)
